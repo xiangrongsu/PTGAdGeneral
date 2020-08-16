bce9dc5385d96b692eacb78efa00f643be6359ea
su <suxiangrong@adinnet.com>
2020-08-16 17:25:58 +0800

    su add gitup

---
+++ PTGAdPublic/PTGAdPublic.h
@@ -0,0 +1,19 @@
+//
+//  PTGAdPublic.h
+//  PTGAdPublic
+//
+//  Created by su on 2020/8/16.
+//  Copyright © 2020 su. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PTGAdPublic.
+FOUNDATION_EXPORT double PTGAdPublicVersionNumber;
+
+//! Project version string for PTGAdPublic.
+FOUNDATION_EXPORT const unsigned char PTGAdPublicVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PTGAdPublic/PublicHeader.h>
+
+
